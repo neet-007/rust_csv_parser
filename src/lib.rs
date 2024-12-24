c90e203b1aadefbc30fff7bd7d61e584bebfa735
neet-007 <moayedeffect@gmail.com>
2024-12-25 02:05:00 +0300

    added tests to cover rest of functionality

--- src/lib.rs
+++ src/lib.rs
@@ -252,4 +252,172 @@ mod tests {
             assert_eq!(token.value, exp);
         }
     }
+
+    #[test]
+    fn parser_trim_fail() {
+        let expect: Vec<Vec<String>> = vec![
+            vec![String::from("1 "), String::from("2 "), String::from("3")],
+            vec![String::from("a"), String::from("b "), String::from("c")],
+        ];
+
+        let str = expect.iter().fold(String::new(), |mut str, row| {
+            str.push_str(
+                row.iter()
+                    .enumerate()
+                    .fold(String::new(), |mut inner_str, (i, field)| {
+                        inner_str.push_str(field.as_str());
+                        if i < row.len() - 1 {
+                            inner_str.push(',');
+                        }
+                        inner_str
+                    })
+                    .as_str(),
+            );
+            str.push('\r');
+            str
+        });
+        let mut parser = CsvParser::from_string(str);
+
+        let tokens = match parser.parse() {
+            Ok(tokens) => tokens,
+            Err(err) => panic!("{err:?}"),
+        };
+
+        for (token, exp) in tokens.iter().zip(expect) {
+            let trimmed_exp = exp
+                .iter()
+                .map(|x| x.trim().to_owned())
+                .collect::<Vec<String>>();
+            assert_ne!(token.value, trimmed_exp);
+        }
+    }
+
+    #[test]
+    fn parser_trim() {
+        let expect: Vec<Vec<String>> = vec![
+            vec![String::from("1 "), String::from("2 "), String::from("3")],
+            vec![String::from("a"), String::from("b "), String::from("c")],
+        ];
+
+        let str = expect.iter().fold(String::new(), |mut str, row| {
+            str.push_str(
+                row.iter()
+                    .enumerate()
+                    .fold(String::new(), |mut inner_str, (i, field)| {
+                        inner_str.push_str(field.as_str());
+                        if i < row.len() - 1 {
+                            inner_str.push(',');
+                        }
+                        inner_str
+                    })
+                    .as_str(),
+            );
+            str.push('\r');
+            str
+        });
+        let mut parser = CsvParser::from_string(str).trim_space(true);
+
+        let tokens = match parser.parse() {
+            Ok(tokens) => tokens,
+            Err(err) => panic!("{err:?}"),
+        };
+
+        for (token, exp) in tokens.iter().zip(expect) {
+            let trimmed_exp = exp
+                .iter()
+                .map(|x| x.trim().to_owned())
+                .collect::<Vec<String>>();
+            assert_eq!(token.value, trimmed_exp);
+        }
+    }
+
+    #[test]
+    fn parser_all_whitespace_fail() {
+        let expect: Vec<Vec<String>> = vec![
+            vec![String::from("1 "), String::from("  "), String::from("3")],
+            vec![String::from("a"), String::from(" "), String::from("")],
+        ];
+
+        let str = expect.iter().fold(String::new(), |mut str, row| {
+            str.push_str(
+                row.iter()
+                    .enumerate()
+                    .fold(String::new(), |mut inner_str, (i, field)| {
+                        inner_str.push_str(field.as_str());
+                        if i < row.len() - 1 {
+                            inner_str.push(',');
+                        }
+                        inner_str
+                    })
+                    .as_str(),
+            );
+            str.push('\r');
+            str
+        });
+        let mut parser = CsvParser::from_string(str);
+
+        let tokens = match parser.parse() {
+            Ok(tokens) => tokens,
+            Err(err) => panic!("{err:?}"),
+        };
+
+        for (token, exp) in tokens.iter().zip(expect) {
+            let trimmed_exp = exp
+                .iter()
+                .map(|x| {
+                    if x.trim().is_empty() {
+                        x.trim().to_owned()
+                    } else {
+                        x.to_string()
+                    }
+                })
+                .collect::<Vec<String>>();
+            assert_ne!(token.value, trimmed_exp);
+        }
+    }
+
+    #[test]
+    fn parser_all_whitespace() {
+        let expect: Vec<Vec<String>> = vec![
+            vec![String::from("1 "), String::from("  "), String::from("3")],
+            vec![String::from("a"), String::from(" "), String::from("")],
+        ];
+
+        let str = expect.iter().fold(String::new(), |mut str, row| {
+            str.push_str(
+                row.iter()
+                    .enumerate()
+                    .fold(String::new(), |mut inner_str, (i, field)| {
+                        inner_str.push_str(field.as_str());
+                        if i < row.len() - 1 {
+                            inner_str.push(',');
+                        }
+                        inner_str
+                    })
+                    .as_str(),
+            );
+            str.push('\r');
+            str
+        });
+        let mut parser = CsvParser::from_string(str).all_whitespace_empty(true);
+
+        let tokens = match parser.parse() {
+            Ok(tokens) => tokens,
+            Err(err) => panic!("{err:?}"),
+        };
+
+        for (token, exp) in tokens.iter().zip(expect) {
+            let trimmed_exp = exp
+                .iter()
+                .map(|x| {
+                    if x.trim().is_empty() {
+                        x.trim().to_owned()
+                    } else {
+                        x.to_string()
+                    }
+                })
+                .collect::<Vec<String>>();
+            assert_eq!(token.value, trimmed_exp);
+        }
+    }
 }
